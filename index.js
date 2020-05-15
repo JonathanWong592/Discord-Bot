3585c329d0856fb9265920ef3048cf4067ea534e
Trevor <trevor@PlutoniumOne.local>
2020-05-15 19:52:41 -0400

    remove / from /join

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on("message", async message => {
   // Let's go with a few common example commands! Feel free to delete or change those.
   if (!message.guild) return;
   
-  if (message.content === '/join') {
+  if (message.content === 'join') {
     // Only try to join the sender's voice channel if they are in one themselves
     if (message.member.voice.channel) {
       const connection = await message.member.voice.channel.join();
