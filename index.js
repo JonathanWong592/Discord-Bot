2d951aa6ee3df37d49b10e609e20e8e7440fc793
JonathanWong592 <jonathanwong592@gmail.com>
2020-05-15 21:06:35 -0400

    Fixed bugs

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// Referencing https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3 as a boilerplate and adding more on top of it
+// referencing https://gist.github.com/eslachance/3349734a98d30011bb202f47342601d3 as boilerplate
 
 // Load up the discord.js library
 const Discord = require("discord.js");
 
-const Music = require("./music.js")
-Music.Hi()
-
-// This is your client. Some people call it `bot`, some people call it `self`,
+// This is your client. Some people call it `bot`, some people call it `self`, 
 // some might call it `cootchie`. Either way, when you see `client.something`, or `bot.something`,
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values.
+// Here we load the config.json file that contains our token and our prefix values. 
 const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} server.`);
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`On ${client.guilds.size} server(s)`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
-  console.log(`New server joined: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!`);
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
   client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
@@ -39,53 +36,62 @@ client.on("guildDelete", guild => {
 
 client.on("message", async message => {
   // This event will run on every single message received, from any channel or DM.
-
+  
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
-
-  // Also good practice to ignore any message that does not start with our prefix,
+  
+  // Also good practice to ignore any message that does not start with our prefix, 
   // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
-
-  // Here we separate our "command" name, and our "arguments" for the command.
+  
+  // Here we separate our "command" name, and our "arguments" for the command. 
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
-
-//   console.log(message.content.split('+'))
-  message.content = message.content.split('+')[1]
-  console.log(message.content)
-  // Let's go with a few common example commands! Feel free to delete or change those.
-  if (!message.guild) return;
   
-  if (message.content === 'join') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    // console.log(message.member.)
-    if (message.member.voice.channel) {
-      const connection = await message.member.voice.channel.join();
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
+  // Let's go with a few common example commands! Feel free to delete or change those.
+  
+  if(command === "ping") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
 
+  console.log(message.content)
+  message.content = message.content.split('+')[1]
 
+  if (message.content === 'join') {
+    try{
+        // Only try to join the sender's voice channel if they are in one themselves
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+      } else {
+        message.reply('You need to join a voice channel first!');
+      }
+    }catch(e){
+      console.log(e)
+    }
+  }
 
   if(message.content === 'music'){
-    if (message.member.voice.channel) {
-        try{
-            const connection = await message.member.voice.channel.join();
-            // connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3',{ volume: 0.5 })
-            connection.play('https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3');
-        }catch(e){
-            console.log("Failed to play music, error: " + e)
-        }
-    } else {
-        message.reply('You need to join a voice channel first!');
+      if (message.member.voice.channel) {
+          try{
+              const connection = await message.member.voice.channel.join();
+              // connection.play('http://www.sample-videos.com/audio/mp3/wave.mp3',{ volume: 0.5 })
+              connection.play('https://ia802508.us.archive.org/5/items/testmp3testfile/mpthreetest.mp3');
+          }catch(e){
+              console.log("Failed to play music, error: " + e)
+          }
+      } else {
+          message.reply('You need to join a voice channel first!');
+      }
     }
-  }
+    
+  
 });
 
 client.login(config.token);
